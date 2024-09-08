ccf3cb805e17878a173f841e99fe10267fba9ec1
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2024-09-07 22:06:41 -0700

    another fix

--- model.py
+++ model.py
@@ -3,7 +3,17 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-db = PostgresqlDatabase('segments', user="segments_owner", password=os.environ.get("PG_PASS"), host="ep-little-union-a682nje5.us-west-2.aws.neon.tech", sslmode="require")
+# Get the endpoint ID from the Neon connection string
+endpoint_id = "ep-little-union-a682nje5"
+
+# Construct the new connection string
+db = PostgresqlDatabase(
+    f"segments?options=endpoint%3D{endpoint_id}",
+    user="segments_owner",
+    password=os.environ.get("PG_PASS"),
+    host=f"{endpoint_id}.us-west-2.aws.neon.tech",
+    sslmode="require"
+)
 
 class Segments(Model):
     id = AutoField()
