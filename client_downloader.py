61620cb9c70d2597fcf86ffda54725e1bf1c127b
Priansh Mittra <81822069+primitt@users.noreply.github.com>
2024-09-07 22:51:52 -0700

    split

--- client_downloader.py
+++ client_downloader.py
@@ -36,11 +36,8 @@ while True:
             break
     for segment in segments_in_local:
         if segment.id > LAST_SEGMENT_PLAYED:
-            print("Playing segment %s" % segment.segment_name)
+            print("Downloading segment %s" % segment.segment_name)
+            s3.download_file("dj3000", f"{segment.segment_name}.wav", f"segment_{segment.id}.wav")
             LAST_SEGMENT_PLAYED = segment.id
-            os.system(f"sudo /home/pi/fm_transmitter/fm_transmitter -f {frequency} -b 300 segment_{segment.id}.wav")
-            # remove the file after playing
-            os.remove(f"segment_{segment.id}.wav")
-            s3.delete_object(Bucket="dj3000", Key=f"{segment.segment_name}.wav")
-            print("Segment %s finished" % segment.segment_name)
+            print("Segment %s finished downloading" % segment.segment_name)
         time.sleep(1)
